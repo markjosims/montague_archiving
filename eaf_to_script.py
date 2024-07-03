39551bb00425aacfbbe70a687141fd23259e7f1e
Mark Simmons <mjsimmons@ucsd.edu>
2024-07-03 07:41:21 -0700

    write output to text file

--- eaf_to_script.py
+++ eaf_to_script.py
@@ -4,7 +4,13 @@ from argparse import ArgumentParser
 from glob import glob
 import os
 
-def write_script(eaf_fp: str) -> str:
+def human_time(timestamp: float) -> str:
+    hours = timestamp//3600
+    minutes = timestamp%3600//60
+    seconds = int(timestamp%60)
+    return f"{hours}:{minutes:0>2d}:{seconds:0>2d}"
+
+def write_script(eaf_fp: str, out_fp: str) -> str:
     eaf = Elan.Eaf(eaf_fp)
     turns = []
     speakers = eaf.get_tier_names()
@@ -12,7 +18,16 @@ def write_script(eaf_fp: str) -> str:
         annotations = eaf.get_annotation_data_for_tier(speaker)
         for start, end, val in annotations:
             turns.append({'start': start, 'end': end, 'text': val, 'speaker': speaker})
-    merge_turn_list(turns)
+    merged_turns = merge_turn_list(turns)
+
+    with open(out_fp, 'w') as f:
+        for turn in merged_turns:
+            speaker = turn['speaker']
+            start = human_time(turn['start'])
+            end = human_time(turn['end'])
+            f.write(f"{speaker}: {start}-{end}")
+            f.write(turn['text'])
+            f.write("\n")
 
 def merge_turn_list(turns: List[Dict[str, str]]) -> List[Dict[str, str]]:
     turns = sorted(turns, key=lambda d:d['start'])
@@ -52,6 +67,12 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             glob_str = os.path.join(input_fp, '*.eaf')
         eafs = glob(glob_str, recursive=args.recursive)
         for eaf_fp in eafs:
-            write_script(eaf_fp)
+            out_fp = eaf_fp.replace('.eaf', '.txt')
+            write_script(eaf_fp, out_fp)
         return 0
-    write_script(input_fp)
+    out_fp = input_fp.replace('.eaf', '.txt')
+    write_script(input_fp, out_fp)
+    return 0
+
+if __name__ == '__main__':
+    main()
